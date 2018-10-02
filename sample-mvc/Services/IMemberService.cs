98618eeb9835bb8321122d95270ecdabafbc259a
Jim <asdfgh1993816@gmail.com>
2018-10-02 23:33:03 +0800

    建立services

---
+++ sample-mvc/Services/IMemberService.cs
@@ -0,0 +1,17 @@
+﻿using sample_mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sample_mvc.Services
+{
+    interface  IMemberService
+    {
+        Member Add(Member member);
+        Member GetById(int Id);
+        IEnumerable<Member> GetAll();
+        bool Update(Member member);
+        bool Delete(int Id);
+    }
+}
---
+++ sample-mvc/Services/MemberSevices.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using sample_mvc.Models;
+using sample_mvc.Repository;
+
+namespace sample_mvc.Services
+{
+    public class MemberSevices : IMemberService
+    {
+        private readonly Repository<Member> repository = new Repository<Member>();
+        public Member Add(Member member)
+        {
+            return repository.Add(member);
+        }
+
+        public bool Delete(int Id)
+        {
+            var data = repository.GetById(Id);
+
+            try
+            {
+                repository.Delete(data);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public IEnumerable<Member> GetAll()
+        {
+            return repository.GetAll();
+        }
+
+        public Member GetById(int Id)
+        {
+            return repository.GetById(Id);
+        }
+
+        public bool Update(Member member)
+        {
+            try
+            {
+                repository.Update(member);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
