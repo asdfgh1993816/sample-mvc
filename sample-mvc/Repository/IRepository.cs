ab90f788888384a89e872dcdbe2423132bda0745
Jim <asdfgh1993816@gmail.com>
2018-10-02 23:21:30 +0800

    建立repository

---
+++ sample-mvc/Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample_mvc.Repository
+{
+    interface IRepository<T> where T :class
+    {
+        T GetById(int id);
+        T GetSingle(Expression<Func<T, bool>> filter);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Get(Expression<Func<T, bool>> filter);
+        T Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ sample-mvc/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using sample_mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace sample_mvc.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected MemberDbContext MemberDbContext
+        {
+            get;
+            private set;
+        }
+        public Repository()
+        {
+            this.MemberDbContext = new MemberDbContext();
+        }
+        public T Add(T entity)
+        {
+            MemberDbContext.Entry(entity).State = EntityState.Added;
+            MemberDbContext.SaveChanges();
+            return entity;
+        }
+
+        public void Delete(T entity)
+        => MemberDbContext.Entry(entity).State = EntityState.Deleted;
+
+        public IEnumerable<T> Get(Expression<Func<T, bool>> filter)
+        => MemberDbContext.Set<T>().Where(filter);
+
+        public IEnumerable<T> GetAll()
+        => MemberDbContext.Set<T>();
+
+        public T GetById(int id)
+         => MemberDbContext.Set<T>().Find(id);
+
+
+        public T GetSingle(Expression<Func<T, bool>> filter)
+         => MemberDbContext.Set<T>().FirstOrDefault(filter);
+
+        public void Update(T entity)
+        {
+            MemberDbContext.Entry(entity).State = EntityState.Modified;
+            MemberDbContext.SaveChanges();
+        }
+    }
+}
